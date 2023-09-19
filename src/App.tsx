214e87bc8ff6f9e405bb115dee22ee8775cd994a
CSychev <claudia.sychev@poly.com>
2023-09-18 23:23:26 -0500

    added routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,22 @@
 import React from "react";
 import Homepage from "./pages/Homepage/Homepage";
+import { Routes, Route, Outlet, Link } from "react-router-dom";
+import CocktailDetails from './pages/CocktailDetails';
+import { Layout } from "./components/Layout";
 
 function App() {
   return (
-    <>
-    <h1>////////////////////////</h1>
-    <Homepage/>
-    </>
+    <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Homepage />} />
+          <Route path="/drink/:id" element={<CocktailDetails />} />
+          
+        </Route>
+      </Routes>
+
   );
 }
+
+
 
 export default App;
--- src/index.tsx
+++ src/index.tsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { HashRouter } from 'react-router-dom';
 export {};
 
 const root = ReactDOM.createRoot(
@@ -10,7 +11,9 @@ const root = ReactDOM.createRoot(
 );
 root.render(
   <React.StrictMode>
-    <App />
+    <HashRouter>
+     <App />
+    </HashRouter>
   </React.StrictMode>
 );
 
